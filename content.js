a2034697c21731098c5daf22bae874175ebb23ff
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2016-06-22 15:51:22 +0500

    started web pack

---
+++ content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Sufiyan on 6/22/2016.
+ */
+module.exports="Hello Sufiyan its Works from content.js";
---
+++ entry.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Sufiyan on 6/22/2016.
+ */
+document.write("Its works.");
+document.write(require("./content.js"));
